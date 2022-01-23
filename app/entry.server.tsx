d7016a186fd6401e33a9c16d8b1dca4b12f70be1
Jacob Ebey <jacob.ebey@live.com>
2022-01-22 17:07:37 -0800

    added content type and transfer encoding

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -18,6 +18,9 @@ export default async function handleRequest(
 ) {
   let dataloader = createServerDataloader(remixContext, request, {}, {});
 
+  responseHeaders.set("Content-Type", "text/html; charset=UTF-8");
+  responseHeaders.set("Transfer-Encoding", "chunked");
+
   return new Promise<void>((resolve) => {
     let didError = false;
     const { pipe, abort } = renderToPipeableStream(
@@ -27,12 +30,18 @@ export default async function handleRequest(
       {
         onCompleteShell() {
           let statusCode = didError ? 500 : responseStatusCode;
-          let headers: Record<string, string[]> = {};
+          let headers: Record<string, string | string[]> = {};
           for (const [key, value] of responseHeaders) {
-            headers[key] = headers[key] || [];
-            headers[key].push(value);
+            if (typeof headers[key] === "string") {
+              headers[key] = [headers[key] as string, value];
+            } else if (Array.isArray(headers[key])) {
+              (headers[key] as string[]).push(value);
+            } else {
+              headers[key] = value;
+            }
           }
-          res.writeHead(statusCode, undefined, headers);
+          console.log(headers);
+          res.writeHead(statusCode, headers);
           pipe(res);
 
           resolve();
